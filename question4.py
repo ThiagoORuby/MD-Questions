fd42f3fd89c81481715fe3c86a9f5a78c6823191
Donvicton <117011568+Donvicton@users.noreply.github.com>
2022-11-06 14:10:06 -0300

    Update question4.py

---
+++ question4.py
@@ -0,0 +1,13 @@
+# QUESTION 4
+# MDC MMC
+
+
+from discrete_math.decomposite import arithmetic
+
+print("##### MMC E MDC #####\n")
+
+number1, number2 = int(input().split(" "))
+res_mdc = decomp_mdc(number1,number2)
+res_mmc = decomp_mmc(number1,number2)
+
+print("MDC :{res_mdc}, MMC : {res_mmc}")
