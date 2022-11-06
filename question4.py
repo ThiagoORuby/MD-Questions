d1c2719b8084d8c0c44ae5cd3b9c5068cd504b1e
Donvicton <117011568+Donvicton@users.noreply.github.com>
2022-11-06 14:38:47 -0300

    Update question4.py

--- question4.py
+++ question4.py
@@ -2,15 +2,15 @@
 # MDC MMC
 
 
-from discrete_math.arithmetic import decomp_mdc
+from discrete_math.arithmetic import decomp_mdc, decomp_mmc
 
-from discrete_math.arithmetic import decomp_mmc
 
 
 print("##### MMC E MDC #####\n")
 
-number1, number2 = int(input().split(" "))
+number1 = int(input("Digite o primeiro número: "))
+number2 = int(input("Digite o segundo número: "))
 res_mdc = decomp_mdc(number1,number2)
 res_mmc = decomp_mmc(number1,number2)
 
-print("MDC :{res_mdc}, MMC : {res_mmc}")
+print(f"MDC :{res_mdc}, MMC : {res_mmc}")
