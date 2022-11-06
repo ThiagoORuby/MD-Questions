c1175a4bc11695c75cf207d6cea2a5c6da01e8cd
Donvicton <117011568+Donvicton@users.noreply.github.com>
2022-11-06 13:58:43 -0300

    Update question3.py

--- question3.py
+++ question3.py
@@ -3,4 +3,12 @@
 
 from discrete_math.decomposite import decompose
 
+print("##### DECOMPOSIÇÃO EM PRIMOS #####\n")
+
+number = int(input("Digite o número: "))
+
+res = decompose(number)
+
+print("{res}")
+
 
